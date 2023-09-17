c0e61653ebdd6ac33e31899fed1413fe009fb8b6
michaelsatumba <mjsatumba@gmail.com>
2023-09-17 15:54:39 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker Registered', registration);
+    })
+    .catch((error) => {
+      console.log('Service Worker Registration Failed', error);
+    });
+  }
+  
